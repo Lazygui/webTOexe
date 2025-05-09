fce6f9c7f64f5310c865b6babc402da23f8ef039
zhouzhijun <15642532452@163.com>
2025-05-09 17:53:54 +0800

    feat(createWindow): 增加状态消息渲染和错误处理
    - 添加 renderMessage 函数，用于向窗口发送状态消息
    - 在创建窗口过程中增加多个状态消息渲染，提升用户体验
    - 优化错误处理，使用 JSON 格式传递错误信息和步骤

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const fs = require('fs');
 const util = require('node:util');
 
 const execAsync = util.promisify(exec);
+const renderMessage = (win, message) => {
+  win.webContents.send('status-message', message);
+}
 const createWindow = async () => {
   const webName = 'www'
   const win = new BrowserWindow({
@@ -19,6 +22,14 @@ const createWindow = async () => {
       webSecurity: false
     }
   })
+  win.webContents.openDevTools(); // 打开浏览器控制台
+
+  win.loadFile(path.join(__dirname, 'inspection.html'));
+  renderMessage(win, {
+    status: 'create',
+    message: "",
+    step: '系统初始化'
+  })
   // 获取Vue项目路径
   const appPath = app.isPackaged
     ? path.join(process.resourcesPath, webName)
@@ -30,37 +41,54 @@ const createWindow = async () => {
   try {
     // 检查 vue项目 文件夹是否存在
     if (!fs.existsSync(appPath)) {
-      throw new Error(`找不到前端项目文件夹: ${appPath}`);
+      throw new Error(JSON.stringify({ step: '程序初始化', message: `找不到前端项目文件夹: ${appPath}` }));
     }
     // 检查 vue项目打包结果是否存在
     if (!fs.existsSync(distPath)) {
+      renderMessage(win, {
+        status: 'create',
+        message: "",
+        step: '检查重要文件'
+      })
       // 检查package.json是否存在
       if (!fs.existsSync(path.join(appPath, 'package.json'))) {
-        throw new Error('找不到Vue项目的package.json文件');
+        throw new Error(JSON.stringify({ step: '检查重要文件', message: '找不到Vue项目的package.json文件' }));
       }
-
+      renderMessage(win, {
+        status: 'create',
+        message: "",
+        step: '程序安装依赖中'
+      })
       // 检查node_modules是否存在，不存在则安装依赖
       if (!fs.existsSync(path.join(appPath, 'node_modules'))) {
-        await execAsync('npm install', { cwd: appPath });
+        try {
+          await execAsync('npm install', { cwd: appPath });
+        } catch (error) {
+          throw new Error(JSON.stringify({ step: '程序安装依赖中', message: '程序安装依赖失败！' }));
+        }
       }
-      // 执行构建命令
-      await execAsync('npm run build', { cwd: appPath });
+      renderMessage(win, {
+        status: 'create',
+        message: "",
+        step: '程序构建中'
+      })
+      try {  // 执行构建命令
+        await execAsync('npm run build', { cwd: appPath });
+      } catch (error) {
+        throw new Error(JSON.stringify({ step: '程序构建中', message: '程序构建失败！' }));
+      }
     }
 
-    // console.log(appPath)
     // 加载构建后的index.html文件
     win.loadFile(path.join(distPath, 'index.html'));
 
   } catch (error) {
-    // 在窗口中显示错误信息
-    win.loadURL(`data:text/html;charset=utf-8,
-      <html>
-        <body>
-          <h1>错误</h1>
-          <pre>${error.message}</pre>
-        </body>
-      </html>
-    `);
+    const info = JSON.parse(error.message);
+    renderMessage(win, {
+      status: 'error',
+      message: info.message,
+      step: info.step
+    })
   }
 }
 app.whenReady().then(() => {
